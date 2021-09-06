59baca47a62a0066fac9755e70eba56b804e1d2e
abdelrahmanElem <89024870+abdelrahmanElem@users.noreply.github.com>
2021-09-06 23:33:59 +0200

    first
    first

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+
+
+# mainDir="C:\\Users\\the_s\\Downloads\\knifelabeling.v1i.yolov5pytorch\\valid\\labels2"
+# def changeClass(loc,classId):
+#     my_list=os.listdir(mainDir)
+#     for file in my_list:
+#         fileLoc= loc+'\\'+file
+#         print(fileLoc)
+#         file1 = open(fileLoc,"r")
+#         str=file1.read()
+#         file1.close()
+#         print(str)
+#         file2= open(fileLoc,"w")
+#         str=str.replace('0','3',1)
+#         file2.write(str)
+#         file2.close()
+# changeClass(mainDir,2)
+
+print("hahah")
+
+
+# python train.py --img 416 --batch 16 --epochs 2 --data '../guns.yaml' --cfg ./models/custom_yolov5s.yaml --weights './runs/train/yol
+# ov5s_results6/weights/best.pt' --name yolov5s_results --nosave --cache
